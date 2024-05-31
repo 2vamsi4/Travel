65ab9e9d08c4e812f47e1564c6e2d02aefd013c9
2vamsi4 <124381887+2vamsi4@users.noreply.github.com>
2024-05-31 11:58:38 +0530

    Add files via upload

---
+++ vamsi/LogoDisplay.java
@@ -0,0 +1,8 @@
+package vamsi;
+
+public class LogoDisplay {
+
+	public static void logoDisplay(String path) {
+				System.out.println("                      "+path+"                     ");
+			}
+}
